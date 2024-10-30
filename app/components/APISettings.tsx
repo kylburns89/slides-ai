72a75d28bd769c61a6e78b4e78f8c13f42d18126
kylburns89 <kylburns89@gmail.com>
2024-10-29 21:46:33 -0500

    Update APISettings.tsx
    Fix type useEffect

--- app/components/APISettings.tsx
+++ app/components/APISettings.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { Settings } from 'lucide-react';
 import { Fragment } from 'react';
